5c977713d2673863ebb0cfdf45c73da1e09508f5
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-08-05 01:54:33 +0900

    💄 UI : 지도 배율 수정

--- src/pages/IntroducePage/IntroducePage.jsx
+++ src/pages/IntroducePage/IntroducePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import NavBar from "../../components/IntroducePage/NavBar";
 import main_center from "../../assets/introduce/intro_center.svg";
 import logo_bottom from "../../assets/introduce/logo_bottom.svg";
@@ -9,8 +9,18 @@ import third from "../../assets/introduce/intro_ex_third.svg";
 import fourth from "../../assets/introduce/intro_ex_fourth.svg";
 import Background2 from "../../components/IntroducePage/Background2";
 
+const fadeInUp = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(50%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
 const IntroducePage = () => {
-  const firstImgRef = useRef(null);
   const secondImgRef = useRef(null);
   const thirdImgRef = useRef(null);
   const fourthImgRef = useRef(null);
@@ -31,17 +41,37 @@ const IntroducePage = () => {
     };
 
     const observer = new IntersectionObserver(handleIntersect, observerOptions);
-    //특정 요소가 뷰포트에 들어오거나 나가는 등 가시성의 변화를 감지할 때 콜백 함수를 호출할 수 있도록 함
-    if (firstImgRef.current) observer.observe(firstImgRef.current);
-    if (secondImgRef.current) observer.observe(secondImgRef.current);
     if (thirdImgRef.current) observer.observe(thirdImgRef.current);
+
+    // 클린업 함수
+    return () => {
+      if (thirdImgRef.current) observer.unobserve(thirdImgRef.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    const observerOptions = {
+      threshold: 0, // 10%가 보일 때 트리거
+    };
+
+    const handleIntersect = entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+        } else {
+          entry.target.classList.remove("in-view");
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, observerOptions);
+    //특정 요소가 뷰포트에 들어오거나 나가는 등 가시성의 변화를 감지할 때 콜백 함수를 호출할 수 있도록 함
+    if (secondImgRef.current) observer.observe(secondImgRef.current);
     if (fourthImgRef.current) observer.observe(fourthImgRef.current);
 
     // 클린업 함수
     return () => {
-      if (firstImgRef.current) observer.unobserve(firstImgRef.current);
       if (secondImgRef.current) observer.unobserve(secondImgRef.current);
-      if (thirdImgRef.current) observer.unobserve(thirdImgRef.current);
       if (fourthImgRef.current) observer.unobserve(fourthImgRef.current);
     };
   }, []);
@@ -58,12 +88,7 @@ const IntroducePage = () => {
         </Center>
         <Mid>
           <div className="first">
-            <img
-              className="firstImg"
-              ref={firstImgRef}
-              src={first}
-              alt="first"
-            />
+            <img className="firstImg" src={first} alt="first" />
           </div>
           <div className="second">
             <img
@@ -134,13 +159,10 @@ const Mid = styled.div`
   }
 
   .firstImg {
+    animation: ${fadeInUp} 1s ease-in-out forwards;
     opacity: 0;
-    transform: translateY(-50%);
-    transition:
-      opacity 1s ease-in-out,
-      transform 1s ease-in-out;
+    transform: translateY(50%);
   }
-
   .secondImg {
     opacity: 0;
     transform: translateX(-100%);
@@ -150,10 +172,9 @@ const Mid = styled.div`
   }
 
   .thirdImg {
-    margin-top: 50px;
-    height: 263px;
+    margin-right: 50px;
     opacity: 0;
-    transform: translateY(100%);
+    transform: translateY(50%);
     transition:
       opacity 1s ease-in-out,
       transform 1s ease-in-out;
@@ -165,11 +186,6 @@ const Mid = styled.div`
     transition:
       opacity 1s ease-in-out,
       transform 1s ease-in-out;
-  }
-
-  .in-view.firstImg {
-    opacity: 1;
-    transform: translateY(0);
   }
 
   .in-view.secondImg {
