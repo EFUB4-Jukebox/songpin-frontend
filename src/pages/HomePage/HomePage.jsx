bc387f3deba25c4c92a3077e4ca6646facab58c6
Gabin Lee <gb94@ewhain.net>
2024-08-06 20:15:08 +0900

    💄 UI: 지도 핀 개수 안내 문구

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-import MapFilter from "../../components/HomePage/MapFilter";
 import SideBar from "../../components/HomePage/SideBar";
 import SideSection from "../../components/common/SideSection";
 import styled from "styled-components";
@@ -59,6 +58,7 @@ const HomePage = () => {
             </PlaceListContainer>
         </SideSection>
         {/* <MapFilter /> */}
+        <AnnounceTxt>지도상에 핀은 최대 300개까지 표시됩니다.</AnnounceTxt>
         </div>
     );
 };
@@ -123,4 +123,18 @@ const PlaceListContainer = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     margin-left: 38px;
+`;
+
+const AnnounceTxt = styled.div`
+    width: 221px;
+    height: 19px;
+    flex-shrink: 0;
+    color: var(--gray02, #747474);
+    font-family: Pretendard;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150%; /* 18px */
+    margin-left: 88px;
+    padding-top: 980px;
 `;
